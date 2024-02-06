af0a6af7725829e56acf03101c2e7b942b290a9b
priyanshurg <priyanshurg98@gmail.com>
2024-02-05 18:21:35 -0600

    publishing

--- backup-pushover.py
+++ backup-pushover.py
@@ -43,35 +43,3 @@ if __name__ == "__main__":
     message = args.message
     send_pushover_notification(api_token, user_key, message)
 
-
-# def send_pushover_notification(api_token, user_key, message, title=None, url=None, url_title=None, priority=None, sound=None):
-#     client = pushover.Client(api_token, user_key)
-
-#     try:
-#         response = client.send_message(
-#             message,
-#             title=title,
-#             url=url,
-#             url_title=url_title,
-#             priority=priority,
-#             sound=sound
-#         )
-#         print("Notification sent successfully. Response:", response)
-#     except pushover.RequestError as e:
-#         print(f"Error sending notification: {e}")
-
-# if __name__ == "__main__":
-#     # Replace these values with your own Pushover API token and user key
-#     api_token = "asqk96egp7miz95oa6v8vavbm4gohi"
-#     user_key = "uqi7ybsbikcszikmgmjxbrc41viq2r"
-
-#     message = "Hello from Python!"
-
-#     # Optional parameters
-#     title = "Notification Title"
-#     url = "https://example.com"
-#     url_title = "Visit Website"
-#     priority = 0  # 0 (normal) or 1 (high)
-#     sound = "pushover"  # Check Pushover app for available sounds
-
-#     send_pushover_notification(api_token, user_key, message, title, url, url_title, priority, sound)
